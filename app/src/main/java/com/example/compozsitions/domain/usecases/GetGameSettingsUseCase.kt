f36e4e5d2d0318156182683abd1d1b0ec994eb71
Ruslan <98656310+Amorusdo@users.noreply.github.com>
2023-01-04 21:34:33 +0600

    Delete GetGameSettingsUseCase.kt

--- app/src/main/java/com/example/compozsitions/domain/usecases/GetGameSettingsUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.compozsitions.domain.usecases
-
-import com.example.compozsitions.domain.models.GameSettings
-import com.example.compozsitions.domain.models.Level
-import com.example.compozsitions.domain.Repository
-
-class GetGameSettingsUseCase(private val repository: Repository) {
-    fun execute(level: Level): GameSettings = repository.getGameSettings(level)
-}
