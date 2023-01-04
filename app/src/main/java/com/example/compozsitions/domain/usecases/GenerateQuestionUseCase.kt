62a330bb448cc8634f9db7e3bf6dcb4111b25ea2
Ruslan <98656310+Amorusdo@users.noreply.github.com>
2023-01-04 21:34:25 +0600

    Delete GenerateQuestionUseCase.kt

--- app/src/main/java/com/example/compozsitions/domain/usecases/GenerateQuestionUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.compozsitions.domain.usecases
-
-import com.example.compozsitions.domain.Repository
-
-class GenerateQuestionUseCase(private val repository: Repository) {
-    fun execute(maxSumValue :Int): Question = repository.generateQuestion(maxSumValue, Utils.COUNT_OF_OPTIONS)
-
-
-}
